72f53f397258d2ce9e89e29b31bd2cf56b655452
chris4824 <chris482422@gmail.com>
2014-11-22 20:37:21 -0500

    Update validus.mk

--- validus.mk
+++ validus.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/validus/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/validus/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-T989 TARGET_DEVICE=SGH-T989 BUILD_FINGERPRINT="samsung/SGH-T989/SGH-T989:4.4/KRT16M/T989UVMC6:user/release-keys" PRIVATE_BUILD_DESC="SGH-T989-user 4.4 KRT16M UVMC6 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-T989 TARGET_DEVICE=SGH-T989 BUILD_FINGERPRINT="samsung/SGH-T989/SGH-T989:5.0/KRT16M/T989UVMC6:user/release-keys" PRIVATE_BUILD_DESC="SGH-T989-user 5.0 KRT16M UVMC6 release-keys"
 
 PRODUCT_NAME := validus_hercules
 PRODUCT_DEVICE := hercules
